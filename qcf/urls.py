9ab02a1fdaaa929e0aceb83ad55b1c1cf03dca50
syn <synwe@yahoo.fr>
2016-06-12 15:49:10 +0000

    New Django 1.9 url format

--- qcf/urls.py
+++ qcf/urls.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from qcf.views import AddPostView
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^', AddPostView.as_view(), name="contact-post"),
-)
+    ]
